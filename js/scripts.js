b09dcdbabce9957373c6e7016a2555c152f099b1
rafaelkellows <rafaelkellows@hotmail.com>
2017-04-06 01:04:32 -0300

    Upload HomePage v2

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,68 @@
-/**
-*@fileOverview Init's component
-* @module Init
-*
-*/
-
-/**
-* Teste Function
-* @exports Init
-* @constructor
-*/
-function init(){
-	/**
-  	* Start Method
-  	* @param {String} txt - value to be alerted
-  	* @memberOf Init#
-  	* @name start
-  	*/
-	this.start=function(txt){
-		alert(txt);
-	};	
-}
-
-
-
-var init= new Init();
-init.start("Testando");
+$(function(){ 
+	(function() {
+		var bmaxima = {
+	        init: function() {
+	            this.carrossel();
+	        },
+	        menu: function() {
+        		var _opnd, _c;
+	        	$('').click(function(){
+	        	
+	        	}).hover(
+	        		function(){	        			
+	        		},
+	        		function(){
+	        		}
+	        	);
+	        },
+	        toTop : function(elem){
+		        var offset = 1;
+		        var duration = 500;
+		        jQuery(window).scroll(function() {
+					if( !$('body main').attr('id') ){ //&& $(window).width() > 1024 
+						if (jQuery(this).scrollTop() > offset) {
+							$('body main').addClass('internal');
+						} else {
+							$('body main').removeClass('internal');
+						}
+					}
+				});
+		        jQuery(elem).click(function(event) {
+					event.preventDefault();
+					jQuery('html, body').animate({scrollTop: 0}, duration);
+					return false;
+		        })
+	        },
+	        carrossel: function() {
+				var owl = $("section.carrossel > div"); 
+				if(!owl.length) return;
+				owl.owlCarousel({
+					autoplay:false,
+					smartSpeed: 600,
+				    loop:true,
+				    margin:0,
+				    nav:false,
+				    responsive:{
+				        0:{
+				            items:1
+				        },
+				        600:{
+				            items:1
+				        },
+				        1000:{
+				            items:1
+				        }
+				    }
+				})
+				if(!owl.length) return -1;
+				owl.owlCarousel({
+					slideSpeed : 1500,
+					paginationSpeed : 1000,
+					singleItem:true,
+					autoPlay:4000
+				}); 
+	        }
+	    }
+		bmaxima.init();
+	})();
+});
